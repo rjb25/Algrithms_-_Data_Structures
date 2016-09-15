3d3f4a375fe57bf202ad75beaed85c0191eab29a
Jason Basanese <jason.basanese@gmail.com>
2016-09-15 14:41:23 -0400

    Format fix

--- RandomData.java
+++ RandomData.java
@@ -10,34 +10,34 @@
 import java.util.*;
 
 public class RandomData {
-	public static void main(String[] args) {
-		Random random = new Random();
-		int[] values = new int[10];
-		int[] taken = new int[values.length];
-		int take = 3;
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] values = new int[10];
+        int[] taken = new int[values.length];
+        int take = 3;
 
-		for (int i = 0; i < values.length; i++) {
-			values[i] = i;
-		}
+        for (int i = 0; i < values.length; i++) {
+            values[i] = i;
+        }
 
-		for (int i = 0; i < taken.length; i++) {
-			taken[i] = values.length;
-		}
+        for (int i = 0; i < taken.length; i++) {
+            taken[i] = values.length;
+        }
 
-		for (int i = 0; i < take; i++) {
-			int randNum = random.nextInt(values.length - i);
-				for (int j = 0; j < taken.length; j++) {
-					if (randNum >= taken[j]) {
-						randNum++;
-					}
-				}
-			taken[randNum] = randNum;
-		}
+        for (int i = 0; i < take; i++) {
+            int randNum = random.nextInt(values.length - i);
+            for (int j = 0; j < taken.length; j++) {
+                if (randNum >= taken[j]) {
+                    randNum++;
+                }
+            }
+            taken[randNum] = randNum;
+        }
 
-		for (int i = 0; i < taken.length; i++) {
-			if(taken[i] != taken.length){
-			System.out.println(taken[i]);
-			}
-		}
-	}
+        for (int i = 0; i < taken.length; i++) {
+            if (taken[i] != taken.length) {
+                System.out.println(taken[i]);
+            }
+        }
+    }
 }
